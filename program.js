17aa009f92c01d6787889fc059fbf251b397d343
aravind15b <aravind15b@gmail.com>
2022-08-25 17:09:35 +0530

    first class

--- program.js
+++ program.js
@@ -3,17 +3,18 @@
 //Primitive types
 
 //string
-var word = "this is a string";
 
+var word = "hello world";
 console.log(word);
 
 //number
-var number_1 = 123;
+var number_1 = 123.333;
 var number_2 = 333;
 var sum = number_1 + number_2;
 console.log(sum);
 
 //boolen
+
 var test = true;
 var test_1 = false;
 console.log(test, test_1);
@@ -30,10 +31,13 @@ var obj = {
   name: "john",
   roll_no: 23,
 };
-console.log(obj);
 
+console.log(obj);
 //Reading value of object
-console.log(obj.name);
+// obj.keyvalue
+
+var de = obj.name;
+console.log(de);
 
 //Reassging value to an object
 
@@ -41,33 +45,62 @@ var obj = {
   name: "john",
   roll_no: 23,
 };
+
 obj.name = "adam";
+
 console.log(obj);
 
 //Adding a new key
+
 var obj = {
   name: "john",
   roll_no: 23,
 };
-
 obj.blood_group = "O +ve";
-
 console.log(obj);
 
 //Deleting a key
-
 var obj = {
   name: "john",
   roll_no: 23,
 };
-delete obj.blood_group;
+
+//delete name of object.nameofkey
+delete obj.name;
 console.log(obj);
 
 //Array
-var arr_string = ["test", "demo"];
-var arr_number = [1, 2, 3];
-var arr_mixed = [1, "string", [1, 2], { name: "tom", test_mark: 90 }];
+var arr = ["test", "123", "+o ve"];
+//0    //1     //2
+//Reading a value of array
+var arr5 = arr[1];
+console.log(arr5);
+//Inserting a new value
+arr.push("a+ ve"); //insert last place
+arr.unshift("93%"); //insert first place
+console.log(arr);
+//delete from arr
+arr.pop(); //delete last place
+console.log(arr);
+arr.shift(); //delete from first place
+console.log(arr);
 
-console.log("This a  array number   " + arr_string);
-console.log("This a  array string  " + arr_number);
-console.log("This a  array mixed   " + arr_mixed);
+//edit array
+//splice
+console.log(arr);
+arr.splice(2, 1, "a +ve", "b +ve", "o -ve");
+console.log(arr);
+
+//function
+//Rules
+//should start small letter
+//should start have not captial
+//keyoword of function small letter
+//no special symbols should give for the function
+function test() {
+  var number_1 = 3;
+  var number_2 = 5;
+  var number_3 = number_1 + number_2;
+  console.log(number_3);
+}
+test(); //executes this only this writtern like this
